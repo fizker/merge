c3464b6ec37f167bb89c8ac4b54fd0a2c6baa7c5
Benjamin Horsleben <benjamin@fizkerinc.dk>
2013-12-19 21:51:40 +0100

    Minor readability fix for the tests.

--- test/merge.js
+++ test/merge.js
@@ -1,5 +1,6 @@
 describe('merge.js', function() {
 	var merge = require('../index')
+
 	describe('When two objects are different', function() {
 		it('should return an object with all properties', function() {
 			expect(merge({ a: 1, b: 2 }, { c: 3, d: 4 }))
@@ -12,18 +13,21 @@ describe('merge.js', function() {
 			expect(a).to.deep.equal({ a: 1, b: 2 })
 		})
 	})
+
 	describe('When two objects share keys', function() {
 		it('should keep the values from the last object', function() {
 			expect(merge({ a: 1, b: 2 }, { b: 3, c: 4 }))
 				.to.deep.equal({ a: 1, b: 3, c: 4 })
 		})
 	})
+
 	describe('When merging multiple objects', function() {
 		it('should merge all', function() {
 			expect(merge({ a: 1 }, { b: 2 }, { c: 3 }))
 				.to.deep.equal({ a: 1, b: 2, c: 3 })
 		})
 	})
+
 	describe('When merging nested objects', function() {
 		it('should merge objects and simple values', function() {
 			var a = { a: { b: 1, c: 2 }, d: 3 }
